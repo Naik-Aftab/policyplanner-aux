d87452bf3897657e19f4c2026af42ad02ac69c91
Naik-Aftab <aftabnaik1999@gmail.com>
2025-11-18 12:16:03 +0530

    enquiry form added

--- src/app/pages/supertopup/quotes/quotes.ts
+++ src/app/pages/supertopup/quotes/quotes.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quotes',
@@ -8,9 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './quotes.scss',
 })
 export class Quotes {
+
+constructor(private router: Router) {}
+
+  goToProposal(plan: any) {
+    this.router.navigate(['supertopup/proposal-form']);
+  }
+
+slides = [
+    {
+      imgSrc: 'assets/quote/slide_img1.png',
+      title: 'High CSR = higher peace of mind',
+      text: 'A higher Claim Settlement Ratio (ideally 95%+) means more claims get paid.'
+    },
+    {
+      imgSrc: 'assets/quote/slide_img2.png',
+      title: 'Fast Claim Processing',
+      text: 'Get your claims settled quickly with our efficient service.'
+    },
+    {
+      imgSrc: 'assets/quote/slide_img3.png',
+      title: 'Expert Guidance',
+      text: 'Our experts will help you choose the best insurance plan.'
+    }
+  ];
+
   plans = [
     {
-      logo: 'assets/quotes/niva.svg',
+      logo: 'assets/quote/national.png',
       name: 'Super Top Up Mediclaim',
       features: [
         'In-patient Treatment',
@@ -49,6 +75,46 @@ export class Quotes {
       deductible: '₹3 Lakhs',
       price: '₹2,833/Year',
       tag: 'Covers Maternity Benefits'
+    },
+      {
+      logo: 'assets/quotes/icici.svg',
+      name: 'Health Booster',
+      features: [
+        'In-patient treatment',
+        'Day care procedures',
+        'Donor expenses'
+      ],
+      cover: '₹15 Lakhs',
+      deductible: '₹3 Lakhs',
+      price: '₹2,833/Year',
+      tag: 'Covers Maternity Benefits'
+    },
+      {
+      logo: 'assets/quotes/icici.svg',
+      name: 'Health Booster',
+      features: [
+        'In-patient treatment',
+        'Day care procedures',
+        'Donor expenses'
+      ],
+      cover: '₹15 Lakhs',
+      deductible: '₹3 Lakhs',
+      price: '₹2,833/Year',
+      tag: 'Covers Maternity Benefits'
+    },  {
+      logo: 'assets/quotes/icici.svg',
+      name: 'Health Booster',
+      features: [
+        'In-patient treatment',
+        'Day care procedures',
+        'Donor expenses'
+      ],
+      cover: '₹15 Lakhs',
+      deductible: '₹3 Lakhs',
+      price: '₹2,833/Year',
+      tag: 'Covers Maternity Benefits'
     }
+
   ];
+
 }
